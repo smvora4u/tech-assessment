a37db2e9bb64c5bb3b443332d85c3c4b17c63402
Sandip Vora <smvora.4u@gmail.com>
2023-01-20 15:07:55 +0530

    Desired function created.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+import { checkEnteredStringHasValidCurlyBrackets } from "./util";
+
+const validStringMsg = "has valid numbers and sequence of '{}' brackets.";
+const inValidStringMsg = "has not valid numbers and sequence of '{}' brackets.";
 
 function App() {
+  const [validationMsg, setValidationMsg] = useState("");
+  const [testString, setTestString] = useState("");
+  const [isValid, setIsValid] = useState();
+
+  const testStringClickHandler = () => {
+    const enteredString = testString.trim();
+    if (enteredString === "") {
+      setValidationMsg(
+        "Please enter a string to test, only space is also not allowed!"
+      );
+      setIsValid(false);
+    }
+
+    if (checkEnteredStringHasValidCurlyBrackets(testString)) {
+      setValidationMsg(enteredString + " - " + validStringMsg);
+      setIsValid(true);
+    } else {
+      setValidationMsg(enteredString + " - " + inValidStringMsg);
+      setIsValid(false);
+    }
+  };
+
   return (
-    <></>
+    <>
+      <div>
+        <label for="validCurlyBrackets">
+          Please enter string to test whether it has valid numbers and sequence
+          of curly brackets.
+        </label>
+      </div>
+      <hr />
+      <input
+        type="text"
+        id="validCurlyBrackets"
+        value={testString}
+        className={isValid === undefined ? "" : isValid ? "valid" : "invalid"}
+        onChange={(e) => setTestString(e.target.value)}
+      />
+      <button onClick={testStringClickHandler}>Test String!</button>
+      <hr />
+      <div className={isValid ? "valid" : "invalid"}>{validationMsg}</div>
+    </>
   );
 }
 
---
+++ src/util/index.js
@@ -0,0 +1,21 @@
+export const checkEnteredStringHasValidCurlyBrackets = (str) => {
+    const enteredString = str.trim();
+    let bracketsInString = [];
+    
+    bracketsInString = enteredString.split('').filter((char) => {
+      return char === "{" || char === "}";
+    });
+  
+    if (bracketsInString.length === 0) {
+      return true;
+    }
+  
+    const openBrackets = bracketsInString.filter(char => char === '{');
+    const closeBrackets = bracketsInString.filter(char => char === '}');
+  
+    if(bracketsInString[0] === '{' && openBrackets.length === closeBrackets.length) {
+      return true;
+    } else {
+      return false;
+    }
+  };

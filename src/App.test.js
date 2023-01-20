482cd546fcd9b01ea571ce79afb80b946446f0d0
Sandip Vora <smvora.4u@gmail.com>
2023-01-20 16:31:21 +0530

    Unit test cases added for the method

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { checkEnteredStringHasValidCurlyBrackets } from "./util";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Check characters in the entered string is have matching words or not", () => {
+  test("{} - True", () => {
+    const result = checkEnteredStringHasValidCurlyBrackets("{}");
+
+    expect(result).toBeTruthy();
+  });
+  test("}{ - False (closed bracket can't proceed all open brackets.)", () => {
+    const result = checkEnteredStringHasValidCurlyBrackets("}{");
+
+    expect(result).toBeFalsy();
+  });
+  test("{{} - False (one bracket pair was not closed)", () => {
+    const result = checkEnteredStringHasValidCurlyBrackets("{{}");
+
+    expect(result).toBeFalsy();
+  });
+  test('"" - True. (no brackets in the string will return True)', () => {
+    const result = checkEnteredStringHasValidCurlyBrackets('""');
+
+    expect(result).toBeTruthy();
+  });
+  test("{abc...xyz} - True (non-bracket characters are ignored appropriately)", () => {
+    const result = checkEnteredStringHasValidCurlyBrackets("{abc...xyz}");
+
+    expect(result).toBeTruthy();
+  });
 });
